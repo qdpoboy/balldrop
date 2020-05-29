2b1c660413ffde591f29c00587b0c55fc0c5f912
qdpoboy <280597526@qq.com>
2020-05-29 23:19:46 +0800

    完成小球加速下落

---
+++ assets/script/ball.js
@@ -0,0 +1,27 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+
+    },
+
+    onLoad() {
+        this.initVelocity = 0;
+    },
+
+    onBeginContact(contact, selfCollider, otherCollider) {
+        let ballRigidBody = selfCollider.node.getComponent(cc.RigidBody);
+        if (this.initVelocity) {
+            //碰装之后，恢复到原始时的线性速度
+            ballRigidBody.linearVelocity = cc.v2(0, this.initVelocity);
+        } else {
+            //初次碰撞时，记录原始的线性速度
+            this.initVelocity = ballRigidBody.linearVelocity.y;
+        }
+    },
+
+
+    // start () {},
+
+    // update (dt) {},
+});
--- assets/script/game.js
+++ assets/script/game.js
@@ -2,11 +2,34 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
+        ballNode: {
+            default: null,
+            type: cc.Node
+        }
     },
 
-    onLoad () {},
+    onLoad() {
+        this.node.on(cc.Node.EventType.TOUCH_START, this.ballDrop, this);
+        this.initPhys();
+    },
 
-    start () {},
+    ballDrop() {
+        let ballRigidBody = this.ballNode.getComponent(cc.RigidBody);
+        ballRigidBody.linearVelocity = cc.v2(0, -2200);
+        console.log('加速下落');
+    },
+
+    initPhys() {
+        this.phys = cc.director.getPhysicsManager();
+        this.phys.enabled = true;
+        this.phys.gravity = cc.v2(0, -2400);
+    },
+
+    start() { },
 
     // update (dt) {},
+
+    onDestroy() {
+        this.node.off(cc.Node.EventType.TOUCH_START, this.ballDrop, this);
+    },
 });
